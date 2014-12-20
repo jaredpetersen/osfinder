1ed8f0fba488cbf5ce9999de612d53da5759d432
Jared Petersen <jpetersen11@wou.edu>
2014-12-20 12:49:41 -0800

    Added CSV Capabilities
    Cleaned up code and added CSV capabilities. Some work still needs to be
    done so that the program filters for Windows XP. As in the previous
    version, it still prints the output of Nmap. This program is still not
    to be used to find Windows XP computers on the network in its current
    state.

--- WindowsXPFinder.java
+++ WindowsXPFinder.java
@@ -1,4 +1,7 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -9,7 +12,7 @@ import java.io.InputStreamReader;
  * that are one the same subnet as the user.
  * 
  * @author Jared Petersen
- * @version 1.0 12/15/2014
+ * @version 2.0 12/19/2014
  * 
  * @param commands Nmap commands that are passed in by the main method
  */
@@ -17,6 +20,11 @@ public class WindowsXPFinder {
 	
 	// ProcessBuilder is used to actually build and run applications (ipconfig and Nmap)
 	ProcessBuilder pb = new ProcessBuilder();
+	
+	// Used to read the output from processes
+	InputStream inputStream;
+    InputStreamReader inputStreamReader;
+    BufferedReader bufferedReader;
 	
 	/**
 	 * The main method. Creates an instance of this class that it then uses to find the subnet
@@ -26,18 +34,26 @@ public class WindowsXPFinder {
 	 */
 	public static void main(String[] args) {		
 		
+		// Print starting message
+		printWelcome();
+		
 		// Create an instance of the class
 		WindowsXPFinder windowsXPFinder = new WindowsXPFinder();
 		
-		// Find the subnet
-		String subnet = windowsXPFinder.getSubnet();
+		// Find the subnet and let the user know
+		String subnet = windowsXPFinder.getSubnet() + "0";
+		printFoundSubnet(subnet);
+		// Determine the name of the CSV output file
+		String fileName = "windowsxpfinder" + subnet + ".csv";
 		
 		// Use the previously determined subnet to run an Nmap command to find all of the OSs
 		// Nmap command should look something like nmap -p 445 --script smb-os-discovery 140.211.114.0/24
-		String finalCommand = "--script smb-os-discovery " + subnet + "0/24";
-		String[] commands = {"cmd.exe", "/c", "nmap ", "-p 445", finalCommand};
-		windowsXPFinder.executeNmapCommand(commands);
-
+		String finalCommand = subnet + "/24";
+		String[] commands = {"cmd.exe", "/c", "nmap ", "-p", "445", "--script", "smb-os-discovery", finalCommand};
+		windowsXPFinder.executeNmapCommand(commands, fileName);
+		
+		// Print the closing message
+		printQuit(fileName);
 	}
 	
 	/**
@@ -51,6 +67,8 @@ public class WindowsXPFinder {
 	{
 		String subnet = "";
 		
+		printSubnetStatus();
+		
 		try 
 		{
 			// Give the ProcessBuilder the ipconfig commands
@@ -59,9 +77,9 @@ public class WindowsXPFinder {
 			Process subnetProcess = pb.start();
 			
 			// Get the data from the process being run
-			InputStream inputStream = subnetProcess.getInputStream();
-	        InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
-	        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+			inputStream = subnetProcess.getInputStream();
+	        inputStreamReader = new InputStreamReader(inputStream);
+	        bufferedReader = new BufferedReader(inputStreamReader);
 	        String line;
 	        
 	        // Begin looping over the data, adding new data lines to the line string
@@ -72,10 +90,10 @@ public class WindowsXPFinder {
 				if (line.length() >= 39)
 				{
 					// Check to see if the line is the default gateway
-				    if (line.substring(0, 39).equals("   Default Gateway . . . . . . . . . : "))
+				    if (line.startsWith("   Default Gateway . . . . . . . . . : "))
 				    {
 				    	// Get the subnet
-				    	subnet = line.substring(39, 51);
+				    	subnet = line.substring(39, 50);
 				    	break;
 				    }
 				}
@@ -86,7 +104,8 @@ public class WindowsXPFinder {
 	        try {
 	            subnetProcess.waitFor();
 	        } catch (InterruptedException e) {
-	            e.printStackTrace();
+	        	// In the case that the process is interrupted, print the error
+	        	e.printStackTrace();
 	        }
 	        
 		}
@@ -106,27 +125,102 @@ public class WindowsXPFinder {
 	 * 
 	 * @param commands Nmap commands that are passed in by the main method
 	 */
-	private void executeNmapCommand(String[] commands)
+	private void executeNmapCommand(String[] commands, String fileName)
 	{
+		String macAddress = null;
+		String hardware = null;
+		String operatingSystem = null;
+		String computerName = null;
+		int xpFound = 0;
+		
+		printNmapStatus();
+		
 		try 
 		{
 			// Give ProcessBuilder the Nmap commands
 			pb.command(commands);
+			pb.redirectErrorStream(true);
 			// Start the process
 			Process nmapProcess = pb.start();
 			
 			// Get the data from the process being run
-			InputStream inputStream = nmapProcess.getInputStream();
-			InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
-			BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+			inputStream = nmapProcess.getInputStream();
+			inputStreamReader = new InputStreamReader(inputStream);
+			bufferedReader = new BufferedReader(inputStreamReader);
 			String line;
+			
+	    	// Prepare to write the CSV file
+	    	File file = new File(fileName);
+	    	FileWriter fileWriter = new FileWriter(file);
+	    	BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+			
+			// Add the CSV File Headings
+			bufferedWriter.write("Operating System, Computer Name, Hardware, MAC Address");
+			bufferedWriter.newLine();
 	        
 	        // Begin looping over the data, adding new data lines to the line string
 	        // Cut out of the loop when the bufferedReader is out of data
 			while ((line = bufferedReader.readLine()) != null)
 			{
-			    System.out.println(line);
+				// Get the MAC Address
+		    	if (line.startsWith("MAC Address: "))
+			    {
+					macAddress = line.substring(13, 30);
+					hardware = line.substring(31);
+			    }
+		    	
+		    	// Get the Operating System
+		    	else if (line.startsWith("|   OS: "))
+			    {
+					operatingSystem = line.substring(8);
+			    }
+		    	
+		    	// Get the Computer Name
+		    	else if (line.startsWith("|   Computer name: "))
+			    {
+					computerName = line.substring(19);
+			    }
+		    	
+		    	// Else -- Do nothing
+			    
+		    	// See if all of the information is there
+		    	if (macAddress != null && operatingSystem != null && computerName != null)
+			    {
+			    	// Check if the computer is Windows XP
+		    		if (operatingSystem.startsWith("Windows 7"))
+	    			{
+				    	// Didn't test for hardware earlier because that's more of an added bonus
+		    			// Testing now so that we can remove the parenthesis that were part of the data
+		    			if (hardware != null)
+			    		{
+			    			// Remove Parenthesis before outputting hardware string
+					    	hardware = hardware.replace("(", "").replace(")", "");
+			    		}
+		    			else
+		    			{
+		    				hardware = "";
+		    			}
+				    	
+				    	// Write the data to the CSV file
+				    	bufferedWriter.write(operatingSystem + ", " + computerName + ", " + hardware + ", " + macAddress);
+				    	bufferedWriter.newLine();
+				    	
+				    	// Increment the Windows XP computers found counter
+				    	xpFound++;
+	    			}
+			    }
+		    	
+		    	// Set variables back to null, next computer
+		    	operatingSystem = null;
+		    	computerName = null;
+		    	hardware = null;
+		    	macAddress = null;
+		    	//System.out.println(line);
+		    	
 			}
+			
+			// Shut the file writer down
+			bufferedWriter.close();
 			
 			// Wait for the Nmap process to finish
 			// Need to have this try/catch statement inside the IOException try/catch
@@ -138,19 +232,21 @@ public class WindowsXPFinder {
 	        	// An exitValue of 0 is complete success
 	            if (exitValue == 0)
 	            {
-	            	System.out.println("\n\nProgram ran sucessfully. Quitting Windows XP Finder.");
+	            	// Print a sucess message and the number of Windows XP computers found
+	            	printSuccess(xpFound);
 	            }
 	            // Anything else can indicate a problem with either ProcessBuilder or the command being run
-	            // Just tell users to try again
 	            else
 	            {
-	            	System.out.println("\n\nNmap command may have failed. Quitting Windows XP Finder.");
+	            	// Just tell users to try again
+	            	printNmapProcessError();
 	            }
 	            
 	        }
 	        catch (InterruptedException e)
 	        {
-	            e.printStackTrace();
+	            // In the case that the process is interrupted, print the error
+	        	e.printStackTrace();
 	        }
 	        
 		}
@@ -160,5 +256,69 @@ public class WindowsXPFinder {
 			e.printStackTrace();
 		}
         
+	}
+	
+	// -------------------------------------------------------------------------------------------------
+	// ------------------------------ Methods used to print notifications ------------------------------
+	// -------------------------------------------------------------------------------------------------
+	
+	/**
+	 * Prints a welcome message to signal the start of the application
+	 */
+	private static void printWelcome()
+	{
+		System.out.println("Starting Windows XP Finder. Please Wait...\n");
+	}
+	
+	/**
+	 * Prints a notification that the application is finding the subnet
+	 */
+	private void printSubnetStatus()
+	{
+		System.out.println("Finding Subnet...");
+	}
+	
+	/**
+	 * Prints the subnet
+	 */
+	private static void printFoundSubnet(String subnet)
+	{
+		System.out.println("Subnet is " + subnet);
+	}
+	
+	/**
+	 * Prints a notification that the Nmap OS scan has begun
+	 */
+	private void printNmapStatus()
+	{
+		System.out.println("Executing Nmap OS Scan...");
+	}
+	
+	/**
+	 * Prints the number of Windows XP machines found by the application
+	 * 
+	 * @param xpFound The number of Windows XP Computers found
+	 */
+	private void printSuccess(int xpFound)
+	{
+		System.out.println(xpFound + " Windows XP Computers were found.");
+	}
+	
+	/**
+	 * Prints a notification that the Nmap command used may have failed
+	 */
+	private void printNmapProcessError()
+	{
+		System.out.println("Nmap command may have failed. Consider restarting Windows XP Finder.");
+	}
+	
+	/**
+	 * Prints a quit message to signal the end of the application
+	 * 
+	 * @param fileName The name of the CSV file that was created
+	 */
+	private static void printQuit(String fileName)
+	{
+		System.out.println("\nData was saved to " + fileName + "\nQuitting Windows XP Finder.");
 	}
 }
